e04ff1bb5428fa415d6a0d4941ed4b47f90627f7
vividsnow <vividsnow@gmail.com>
2026-06-23 23:27:29 +0300

    v0.12: optional arena_cap constructor arg decouples string arena from max_entries

--- lib/Data/HashMap/Shared/SS.pm
+++ lib/Data/HashMap/Shared/SS.pm
@@ -2,7 +2,7 @@ package Data::HashMap::Shared::SS;
 use strict;
 use warnings;
 use Data::HashMap::Shared;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 sub import {
     $^H{"Data::HashMap::Shared::SS/shm_ss_put"}        = 1;
